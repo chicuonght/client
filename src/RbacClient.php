2d8818d1ba2967a3cf0293760ca76fddd9719e45
Thanh Pham <thanh.pham@seldatinc.com>
2017-06-03 10:30:39 +0700

    WoohooLabs client

--- src/RbacClient.php
+++ src/RbacClient.php
@@ -2,7 +2,15 @@
 
 namespace yii2rbac\client;
 
+use Http\Adapter\Guzzle6\Client;
+use GuzzleHttp\Psr7\Request;
+use WoohooLabs\Yang\JsonApi\Client\JsonApiClient;
+use WoohooLabs\Yang\JsonApi\Request\JsonApiRequestBuilder;
+use WoohooLabs\Yang\JsonApi\Request\ResourceObject;
+use WoohooLabs\Yang\JsonApi\Response\JsonApiResponse;
+
 /**
+ * RBAC Client for Lumen Framework 5.4
  * Created by PhpStorm.
  * User: thanh
  * Date: 2017-06-02
@@ -17,36 +25,51 @@ class RbacClient{
         'Content-Type' => 'application/vnd.api+json'
     ];
 
+    public $response = [];
+
+    public $error;
+
     public function __construct(string $url, string $jwtToken)
     {
         $this->url = $url;
         $this->headers['Authorization'] = sprintf('Bearer %s', $jwtToken);
+
+
         $this->client = new \GuzzleHttp\Client();
     }
 
     public function check(string $permission, array $params = []){
-        $method = 'POST';
-        $body = [
-            'data' => [
-                "type" => "Authorize",
-                "attributes" => [
-                    'permission' => $permission,
-                    'data' => $params,
-                ],
-            ],
-        ];
+        $result = false;
+        $resource = new ResourceObject("Authorize");
+        $resource ->setAttributes([
+            'permission' => $permission,
+            'data' => $params,
+        ]);
+        $guzzleClient = Client::createWithConfig([]);
+        // Instantiate the syncronous JSON:API Client
+        $this->client = new JsonApiClient ($guzzleClient);
+        $request = new Request('', '');
+        $requestBuilder = new JsonApiRequestBuilder($request);
+        $requestBuilder->setProtocolVersion("2.0")
+            ->setMethod("POST")
+            ->setUri($this->url)
+            ->setHeader("Authorization", $this->headers['Authorization'])
+            ->setHeader("Content-Type", $this->headers['Content-Type'])
+            ->setJsonApiBody($resource);
+        $request = $requestBuilder->getRequest();
 
-        $options  = [
-            'headers' => $this->headers,
-            'body' => json_encode($body)
-        ];
+        $psr7Response  = $this->client->sendRequest($request);
 
-        $response  = $this->client->request($method, $this->url, $options);
-
-        if($response->getStatusCode()  == 200){
-            return $response->getBody();
-
+        $response = new JsonApiResponse($psr7Response);
+        $document = $response->document();
+        if($response->isSuccessfulDocument()){
+            $result = true;
+            $this->response =  $document->primaryResource();
+        }else{
+            $body =  $psr7Response->getBody();
+            $this->response = \GuzzleHttp\json_decode($body);
         }
-        return false;
+
+        return $result;
     }
 }
